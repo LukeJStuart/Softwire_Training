878c7114bf05f38a5502c4f9c0d786cde0acac21
LukeJStuart <luke.stuart@sky.com>
2023-09-20 10:33:21 +0100

    implementing basic fizzbuzz

--- FizzBuzzCSharpProject/Program.cs
+++ FizzBuzzCSharpProject/Program.cs
@@ -1,1 +1,40 @@
-﻿Console.WriteLine("Hello, World!");
+﻿namespace FizzBuzzCSharpProject
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            FizzBuzzIterate(100);
+        }
+
+        private static string FizzBuzz(int x)
+        {
+            if (x % 3 == 0 && x % 5 == 0)
+            {
+                return "FizzBuzz";
+            }
+            else if (x % 3 == 0)
+            {
+                return "Fizz";
+            }
+            else if (x % 5 == 0)
+            {
+                return "Buzz";
+            }
+            else
+            {
+                return x.ToString();
+            }  
+        }
+
+        private static void FizzBuzzIterate(int x)
+        {
+            for (var i = 1; i <= x; i++)
+            {
+                Console.WriteLine(FizzBuzz(i));
+            }
+        }
+    }
+
+}
+
