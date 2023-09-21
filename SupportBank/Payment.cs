f9ac778cba4cc9756cef275f7d35b61d2520667b
LukeJStuart <luke.stuart@sky.com>
2023-09-21 12:28:42 +0100

    creating dictionary of accounts for each unique name from the dataset

---
+++ SupportBank/Payment.cs
@@ -0,0 +1,10 @@
+﻿namespace SupportBank;
+
+public class Payment
+{
+    public string Date { get; set; }
+    public string From { get; set; }
+    public string To { get; set; }
+    public string Narrative { get; set; }
+    public string Amount { get; set; }
+}
--- SupportBank/Program.cs
+++ SupportBank/Program.cs
@@ -7,23 +7,23 @@ namespace SupportBank
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine(GetRecords());
+            var records = GetRecords().ToList();
+            var accounts = records
+                .Select(r => r.From)
+                .Concat(
+                    records.Select(r => r.To)
+                )
+                .Distinct()
+                .ToDictionary(uniqueName => uniqueName, _ => 0);
+            
+            Console.WriteLine(accounts["Jon A"]);
         }
 
         private static System.Collections.Generic.IEnumerable<SupportBank.Payment> GetRecords()
         {
             using var reader = new StreamReader(@"C:\Users\lukstu\Documents\Softwire_Training\Transactions2014.csv");
             using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
-            return csv.GetRecords<Payment>();
+            return csv.GetRecords<Payment>().ToList();
         }
-    }
-
-    public abstract class Payment
-    {
-        public string? Date { get; set; }
-        public string? From { get; set; }
-        public string? To { get; set; }
-        public string? Narrative { get; set; }
-        public string? Amount { get; set; }
     }
 }
