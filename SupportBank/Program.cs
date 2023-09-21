cee10327e1740485b6c98754d19aae4072ec8bb3
LukeJStuart <luke.stuart@sky.com>
2023-09-21 17:11:19 +0100

    added List All functionality and List[] functioanlity and take user input for these via an infinite loop

--- SupportBank/Program.cs
+++ SupportBank/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Globalization;
+using System.Text.RegularExpressions;
 using CsvHelper;
 
 namespace SupportBank
@@ -14,9 +15,75 @@ namespace SupportBank
                     records.Select(r => r.To)
                 )
                 .Distinct()
-                .ToDictionary(uniqueName => uniqueName, _ => 0);
+                .ToDictionary(uniqueName => uniqueName, _ => 0.0);
+
+            foreach (var p in records)
+            {
+                accounts[p.From] -= double.Parse(p.Amount);
+                accounts[p.To] += double.Parse(p.Amount);
+
+            }
             
-            Console.WriteLine(accounts["Jon A"]);
+            var commandRegex = new Regex("List\\[[^\\]]*\\]", RegexOptions.IgnoreCase);
+            
+            // User Input
+            while (true)
+            {
+                Console.Write("Enter 'List All' to output all account balances or 'List[");
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.Write("Account");
+                Console.ResetColor();
+                Console.WriteLine("]' to show all transactions for a given account.");
+                
+                Console.WriteLine();
+                
+                Console.Write("Enter Command: ");
+                var userInput = Console.ReadLine();
+
+                if (userInput is not null)
+                {
+                    if (userInput == "List All")
+                    {
+                        Console.WriteLine("Listing all accounts:");
+                        Console.WriteLine();
+            
+                        Console.WriteLine("{0,-10}{1,10}", "Name", "Balance");
+                        Console.WriteLine();
+            
+                        foreach (var a in accounts)
+                        {
+                            Console.WriteLine("{0,-10}{1,10:\u00a3#.##}", a.Key, a.Value);
+                        }
+                    }
+                    else if (commandRegex.IsMatch(userInput))
+                    {
+                        if (accounts.Select(a => a.Key).Contains(userInput[5..^1]))
+                        {
+                            var chosenName = userInput[5..^1];
+                            Console.WriteLine("Listing all transactions for " + chosenName + ":");
+                            Console.WriteLine();
+            
+                            Console.WriteLine("{0,-10}{1,-10}{2,-15}{3,-15}{4,-30}", "Date", "Amount", "From", "To", "Narrative");
+                            Console.WriteLine();
+
+                            foreach (var r in records.Where(r => r.From == chosenName || r.To == chosenName))
+                            {
+                                Console.WriteLine("{0,-10}{1,-10:\u00a30.00}{2,-15}{3,-15}{4,-30}", r.Date, double.Parse(r.Amount), r.From, r.To, r.Narrative);
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid Account Name.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid Command.");
+                    }
+
+                    Console.WriteLine();
+                }
+            }
         }
 
         private static System.Collections.Generic.IEnumerable<SupportBank.Payment> GetRecords()
