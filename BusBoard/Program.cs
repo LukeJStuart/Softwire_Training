7a226f0a7046e33604a2b3ec6abf67cd040f3aff
LukeJStuart <luke.stuart@sky.com>
2023-09-27 09:30:54 +0100

    refactor so API calls are in separate methods

--- BusBoard/Program.cs
+++ BusBoard/Program.cs
@@ -12,17 +12,33 @@ internal static class Program
         // Example bus stop code: 490008660N
         var userInput = Console.ReadLine();
         Console.WriteLine();
-        var apiEndPoint = "https://api.tfl.gov.uk/StopPoint/" + userInput + "/Arrivals";
+        
+        var nextFiveBuses = GetNextFiveBusesForStop(userInput!);
+        
+        Console.WriteLine("{0,-10}{1,-30}{2,-10}", "Route", "Destination", "Minutes");
+        foreach (var b in nextFiveBuses)
+        {
+            Console.WriteLine("{0,-10}{1,-30}{2,-10}", b.Route, b.Destination, int.Parse(b.ArrivalTime) / 60);
+        }
+    }
+
+    private static string? GetApiResponseContent(string apiEndPoint)
+    {
         var client = new RestClient(apiEndPoint);
         var request = new RestRequest("/");
         request.AddHeader("Content-Type", "application/json");
         var response = (RestResponse)client.Execute<RestRequest>(request);
         var content = response.Content;
-        var tflResponse = JsonConvert.DeserializeObject<List<IncomingBus>>(content!)!.OrderBy(bus => int.Parse(bus.ArrivalTime)).Take(5);
-        Console.WriteLine("{0,-10}{1,-30}{2,-10}", "Route", "Destination", "Minutes");
-        foreach (var b in tflResponse)
-        {
-            Console.WriteLine("{0,-10}{1,-30}{2,-10}", b.Route, b.Destination, int.Parse(b.ArrivalTime) / 60);
-        }
+        return content;
+    }
+
+    private static IEnumerable<IncomingBus> GetArrivalsForStop(string stopCode)
+    {
+        var responseContent = GetApiResponseContent("https://api.tfl.gov.uk/StopPoint/" + stopCode + "/Arrivals");
+        return JsonConvert.DeserializeObject<List<IncomingBus>>(responseContent!)!;
+    }
+    private static IEnumerable<IncomingBus> GetNextFiveBusesForStop(string stopCode)
+    {
+        return GetArrivalsForStop(stopCode).OrderBy(bus => int.Parse(bus.ArrivalTime)).Take(5);
     }
 }
